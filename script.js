a94a55cfc28155ad0ef588a57e805d05cbb92ffa
azulejo1237 <alejandrogpalazzo@gmail.com>
2022-08-10 18:01:20 -0300

    Carrito-modal y inicio-De-Sesion-Modale arreglo

--- script.js
+++ script.js
@@ -56,14 +56,6 @@ fetch("./json/productos.json")
         })
     })
 
-// // Agregar al carrito icono
-// const iconoCarrito = document.getElementById("icono__carrito")
-// iconoCarrito.addEventListener("click", (e) => {
-//     e.preventDefault()
-//     Swal.fire({html:''})
-// })
-
-
 // Boton comprar
 const comprarButton = document.querySelector('.comprarButton')
 comprarButton.addEventListener('click', comprarButtonClicked)
@@ -111,13 +103,13 @@ function agregarItemsAlCarrito(productoTitulo, productoPrecio, productoImg) {
     const carritoDeCompraContenedor = `
         <div class="row shoppingCartItem item">
         <div class="col-6">
-            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <div class="shopping-cart-item d-flex align-items-center h-100 pb-2 pt-3">
                 <img src=${productoImg} class="shopping-cart-image">
                 <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${productoTitulo}</h6>
             </div>
         </div>
         <div class="col-2">
-            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <div class="shopping-cart-price d-flex align-items-center h-100 pb-2 pt-3">
             <div class="item-details">
                 <p class="item-price mb-0 shoppingCartItemPrice">${productoPrecio}</p>
                 </div>
@@ -125,7 +117,7 @@ function agregarItemsAlCarrito(productoTitulo, productoPrecio, productoImg) {
         </div>
         <div class="col-4">
             <div
-                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 pb-2 pt-3">
                 <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
                     value="1">
                 <button class="btn btn-danger buttonDelete" type="button">X</button>
